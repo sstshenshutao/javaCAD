71dbd8e928e633ac2da87708a0b54c0722d10f92
sstshenshutao <sstbage@gmail.com>
2018-03-21 18:37:44 +0100

    sst finish B1-B3

---
+++ src/model/angled/TriangleElement.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package model.angled;
+
+import model.GeometricModelElement;
+
+/**
+ * @author Shutao Shen
+ *
+ */
+public class TriangleElement extends AngledGeometricElement {
+
+	/**
+	 * 
+	 */
+	public TriangleElement() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see model.GeometricModelElement#getJSONClassName()
+	 */
+	@Override
+	protected String getJSONClassName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see model.GeometricModelElement#cloneElement()
+	 */
+	@Override
+	public GeometricModelElement cloneElement() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
