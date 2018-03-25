f42f2a5e5c4dee6e2b1fddcf7e838a36733248a6
sstshenshutao <sstbage@gmail.com>
2018-03-25 23:15:17 +0200

    finish AngledGeometricElement

--- src/model/angled/AngledGeometricElement.java
+++ src/model/angled/AngledGeometricElement.java
@@ -13,26 +13,102 @@ public abstract class AngledGeometricElement extends GeometricModelElement {
 
 	@Override
 	protected void calculateMove(double xDirection, double yDirection) {
-
+		Point[] points = this.getPoints();
+		for (Point point : points) {
+			point.setX(point.getX() + xDirection);
+			point.setY(point.getY() +yDirection);
+		}
 	}
 
 	@Override
 	protected void calculateRotation(double angle) {
+		Point[] points = this.getPoints();
+		//get the max x and max y
+		double maxX=Double.MIN_VALUE;
+		double maxY=Double.MIN_VALUE;
+		for (int i=0;i<points.length;i++) {
+			if(points[i].getX()>maxX) maxX=points[i].getX();
+			if(points[i].getY()>maxY) maxY=points[i].getY();
+		}
+		//get the min x and min y
+		double minX=Double.MAX_VALUE;
+		double minY=Double.MAX_VALUE;
+		for (int i=0;i<points.length;i++) {
+			if(points[i].getX()<minX) minX=points[i].getX();
+			if(points[i].getY()<minY) minY=points[i].getY();
+		}
+		//centerpoint of this geometric
+		double centerX = (maxX+minX)/2;
+		double centerY = (maxY+minY)/2;
+		
+//		x'=(x-cx)cosa+(y-cy)(-sina)+cx
+//		y'=(x-cx)sina+(y-cy)cosa+cy
+		for (Point point : points) {
+			point.setX((point.getX() - centerX) * Math.cos(angle) + (point.getY() - centerY) * (-1) * Math.sin(angle)+ centerX);
+			point.setY((point.getX() - centerX) * Math.sin(angle) + (point.getY() - centerY) *  Math.cos(angle)+ centerY);
+		}
 
 	}
 
 	@Override
 	protected void calculateScale(double factor) {
-		//scale sst
 		Point[] points = this.getPoints();
+		//get the max x and max y
+		double maxX=Double.MIN_VALUE;
+		double maxY=Double.MIN_VALUE;
+		for (int i=0;i<points.length;i++) {
+			if(points[i].getX()>maxX) maxX=points[i].getX();
+			if(points[i].getY()>maxY) maxY=points[i].getY();
+		}
+		//get the min x and min y
+		double minX=Double.MAX_VALUE;
+		double minY=Double.MAX_VALUE;
+		for (int i=0;i<points.length;i++) {
+			if(points[i].getX()<minX) minX=points[i].getX();
+			if(points[i].getY()<minY) minY=points[i].getY();
+		}
+		//centerpoint of this geometric
+		double centerX = (maxX+minX)/2;
+		double centerY = (maxY+minY)/2;
 		for (Point point : points) {
-			point.setX(point.getX() * factor);
-			point.setY(point.getY() * factor);
+			point.setX((point.getX() - centerX) * factor + centerX);
+			point.setY((point.getY() - centerY) * factor + centerY);
 		}
 	}
 
 	@Override
 	protected void calculateMirroring(String axis) {
-
+		if (!axis.equalsIgnoreCase("x") && !axis.equalsIgnoreCase("y")) return ;
+		Point[] points = this.getPoints();
+		//get the max x and max y
+		double maxX=Double.MIN_VALUE;
+		double maxY=Double.MIN_VALUE;
+		for (int i=0;i<points.length;i++) {
+			if(points[i].getX()>maxX) maxX=points[i].getX();
+			if(points[i].getY()>maxY) maxY=points[i].getY();
+		}
+		//get the min x and min y
+		double minX=Double.MAX_VALUE;
+		double minY=Double.MAX_VALUE;
+		for (int i=0;i<points.length;i++) {
+			if(points[i].getX()<minX) minX=points[i].getX();
+			if(points[i].getY()<minY) minY=points[i].getY();
+		}
+		double axisY = (maxX+minX)/2;
+		double axisX = (maxY+minY)/2;
+		if (axis.equalsIgnoreCase("x")) {
+			for(Point p: points) {
+				//y
+				p.setY(axisX*2-p.getY());
+			}
+		}else if(axis.equalsIgnoreCase("y")){
+			for(Point p: points) {
+				//x
+				p.setX(axisY*2-p.getX());
+			}
+		}
 	}
+	
+	
+	
 }
