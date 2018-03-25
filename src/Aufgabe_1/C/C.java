b5844d326fc7abc24f8c3499a7cf79c7a2ae4364
sstshenshutao <sstbage@gmail.com>
2018-03-25 23:27:56 +0200

    C.java

--- src/Aufgabe_1/C/C.java
+++ src/Aufgabe_1/C/C.java
@@ -13,6 +13,41 @@ import data.ListItem;
  *            Generic Type
  */
 public class C<T> {
+	
+	public void hilfecombine(T[] arr1, T[] arr2, T[] arr3, Comparator<T> cmp, int i, int j)
+	 {
+	  if (arr1 == null)
+	  {
+	   if (arr2 == null)
+	    arr3 = null;
+	   else
+	    arr3 = arr2;
+	  }
+	  else if (arr2 == null)
+	   arr3 = arr1;
+	  else if (i+j >= arr3.length)
+	   return;
+	  else if (i >= arr1.length)
+	  { 
+	   arr3[i+j] = arr2[j];
+	   hilfecombine(arr1, arr2, arr3, cmp, i, j + 1);
+	  }
+	  else if (j >= arr2.length)
+	  {
+	   arr3[i + j] = arr1[i];
+	  hilfecombine(arr1, arr2, arr3, cmp, i + 1, j);
+		}
+	   else if (cmp.compare(arr1[i], arr2[j]) <= 0)
+		{
+		 arr3[i + j] = arr1[i];
+		 hilfecombine(arr1, arr2, arr3, cmp, i + 1, j);
+		}
+		 else
+		 {
+		 arr3[i + j] = arr2[j];
+		 hilfecombine(arr1, arr2, arr3, cmp, i, j + 1);
+		}
+
 	/**
 	 * Die Methode kombiniert zwei nach dem übergebenem Comparator aufsteigend sortierte Arrays zu einem Array, das
 	 * wiederum gemäß des Comparators aufsteigend sortiert ist, und gibt dieses zurück. Wird für cmp eine null-Referenz
@@ -28,10 +63,22 @@ public class C<T> {
 	 *            the comparator
 	 * @return a combined Array with all elements of the two given arrays.
 	 */
-	public T[] combine(T[] arr1, T[] arr2, Comparator<T> cmp, Class<?> type) throws IllegalArgumentException {
-		// TODO Your task
-		return null;
-	}
+	  public T[] combine(T[] arr1, T[] arr2, Comparator<T> cmp, Class<?> type) throws IllegalArgumentException {
+		  if (cmp == null || type == null)
+		   throw new IllegalArgumentException();
+		  
+		  for (int i = 0; i < arr1.length - 1; i++)
+		  {
+		   if (cmp.compare(arr1[i], arr1[i + 1]) > 0)
+		    throw new IllegalArgumentException();
+		  }
+		  
+		  for (int i = 0; i < arr2.length - 1; i++)
+		  {
+		   if (cmp.compare(arr2[i], arr2[i + 1]) > 0)
+		    throw new IllegalArgumentExcept;
+		  }
+	  }
 
 	/**
 	 * Die Methode fügt die komplette Liste head an den Anfang der Liste lst an und gibt den neuen Listenkopf zurück.
@@ -46,9 +93,16 @@ public class C<T> {
 	 * @return the list with the new head in front
 	 */
 	public ListItem<T> insertHead(ListItem<T> lst, ListItem<T> head) {
-		// TODO Your task
-		return null;
+		if (lst == null && head == null) {
+			throw new IllegalArgumentException();
+		}
+		if (lst == null) {
+			return head;
+		}
+		head.insert(lst.key);
+		return insertHead(lst.next, head);
 	}
+
 
 	/**
 	 * Die Methode entfernt das letzte Listenelement aus der Liste lst und gibt die resultierende Liste zurück. Wenn für
@@ -60,8 +114,18 @@ public class C<T> {
 	 * @return the list without the last element
 	 */
 	public ListItem<T> removeLast(ListItem<T> lst) {
-		// TODO Your task
-		return null;
+		if (lst == null)
+			return lst;
+		else {
+			ListItem<T> p = lst;
+			ListItem<T> last = p;
+			while (p.next != null) {
+				last = p;
+				p = p.next;
+			}
+			last.next = null;
+			return lst;
+		}
 	}
 
 	/**
@@ -73,8 +137,13 @@ public class C<T> {
 	 * @return the inverted list
 	 */
 	public ListItem<T> invert(ListItem<T> lst) {
-		// TODO Your task
-		return null;
+		if(lst == null && lst.next == null){
+			return lst;
+		}
+		ListItem<T> newlst = invert(lst.next);
+		lst.next.next = lst;
+		lst.next = null;
+		return newlst;
 	}
 
 	/**
@@ -89,8 +158,17 @@ public class C<T> {
 	 * @return the combined list
 	 */
 	public ListItem<T> combineLists(ListItem<T> lst1, ListItem<T> lst2) {
-		// TODO Your task
-		return null;
+		  if (lst1 == null)
+		   return lst2;
+		  else if (lst2 == null)
+		   return lst1;
+		  else
+		  {
+		   ListItem<T> lst = new ListItem<T>(lst2.key);
+		   lst.next = new ListItem<T>(lst1.key);
+		   lst.next.next = combineLists(lst1.next, lst2.next);
+		   return lst;
+		  }
 	}
 
 	/**
@@ -105,7 +183,19 @@ public class C<T> {
 	 * @throws IllegalArgumentException
 	 */
 	public ListItem<T> arrayIntoList(T[] arr) throws IllegalArgumentException {
-		// TODO Your task
-		return null;
+		if(arr == null) {
+			throw new IllegalArgumentException();
+		}	
+		ListItem<T> newlst = new ListItem<T>(arr[0]);
+		ListItem<T> p = newlst;
+		if(arr.length == 0) {
+			return null;
+		}
+		for (int i = 1; i < arr.length; i++) {
+			ListItem<T> item = new ListItem<T>(arr[i]);
+			p.next = item;
+			p = p.next;
+		}
+		return newlst;
 	}
 }
