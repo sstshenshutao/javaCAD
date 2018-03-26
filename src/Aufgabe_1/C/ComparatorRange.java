053a9140487f97c1cc85159e7067158042e816b7
chenachen <gilbert_twins@qq.com>
2018-03-26 19:48:27 +0800

    更新 ComparatorRange.java

--- src/Aufgabe_1/C/ComparatorRange.java
+++ src/Aufgabe_1/C/ComparatorRange.java
@@ -3,6 +3,8 @@ package Aufgabe_1.C;
 import java.util.Comparator;
 
 import model.angled.AngledGeometricElement;
+import model.Point;
+import util.Util;
 
 /**
  * Die Klasse implementiert einen java.util.Comparator, welcher den Vergleich anhang des Umfangs durchführt. Dabei ist
@@ -14,9 +16,28 @@ public class ComparatorRange implements Comparator<AngledGeometricElement> {
 	 * implementiert. Die Methode führt den Vergleich anhand des Umfangs durch. Dabei ist ein kleinerer Umfang als
 	 * kleiner zu bewerten.
 	 */
+	public float getLength(Point[] points) {
+	    double distance = 0;
+	    for (int i = 0; i < points.length; i++) {
+	        Point itemA = points[i];
+		    int index = i+1;
+		    if (i == points.length - 1) {
+			    index = 0;
+			}
+		    Point itemB = points[index];
+            distance += Math.sqrt(Math.pow((itemA.getX()-itemB.getX()), 2) + Math.pow((itemA.getY()-itemB.getY()), 2));
+	    }
+		return distance;
+	}
+		
 	@Override
 	public int compare(AngledGeometricElement elem1, AngledGeometricElement elem2) {
-		// TODO Your task
-		return 0;
+	    if(elem1.getLength == elem2.getLength) {
+		   return 0;
+	    }else if(elem1.getLength > elem1.getLength) {    	
+		         return 1;
+	    }else {  
+		       return -1;  
+		}  
 	}
 }
