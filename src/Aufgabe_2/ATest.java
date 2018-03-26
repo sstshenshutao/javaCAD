17f3b1781f456e70069988c36c733346a72f528a
sstshenshutao <sstbage@gmail.com>
2018-03-26 19:47:00 +0200

    Atest and Dtest贤姐

--- src/Aufgabe_2/ATest.java
+++ src/Aufgabe_2/ATest.java
@@ -1,31 +1,63 @@
 package Aufgabe_2;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Comparator;
+
+import data.ListItem;
+import Aufgabe_1.A.A;
+import Aufgabe_1.V.ComparatorInteger;
+import model.Point;
+import model.GeometricModelElement;
+import model.angled.AngledGeometricElement;
+import model.angled.SquareElement;
+import model.angled.RectangleElement;
+import model.round.CircleElement;
+import model.round.RoundGeometricElement;
 
 public class ATest {
 
 	/*****************************************************************
 	 * invertTriples
 	 *****************************************************************/
-
+	private A<Integer> a = new A<>();
+	private Integer[] arr1 = {1,2,3,4,5,6,7};
+	private Integer[] arr2 = {1,2,3};
+	private Integer[] arr3 = {1,2};
+	private Integer[] arr4 = null;
+	
 	@Test
 	public void invertTriples_Test_1() {
-
+		Integer[] iArr = a.invertTriples(arr1);
+        Integer[] eArr = {3,2,1,6,5,4,7};
+		
+        Assert.assertTrue(eArr.equals(iArr));
 	}
 
 	@Test
 	public void invertTriples_Test_2() {
-
+		Integer[] iArr = a.invertTriples(arr2);
+        Integer[] eArr = {3,2,1};
+		
+        Assert.assertTrue(eArr.equals(iArr));    
 	}
 
 	@Test
 	public void invertTriples_Test_3() {
-
+		Integer[] iArr = a.invertTriples(arr3);
+        Integer[] eArr = {1,2};
+		
+        Assert.assertTrue(eArr.equals(iArr));        
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void invertTriples_Test_4() {
-
+		
+		a.invertTriples(arr4);
 	}
 
 	/*****************************************************************
@@ -34,17 +66,42 @@ public class ATest {
 
 	@Test
 	public void insertSingleHead_Test_1() {
+		ListItem<Integer> lst= new ListItem<>(4);
+		lst.insert(5);
+		lst.insert(3);
+		
+		ListItem<Integer> newLst = a.insertSingleHead(lst,9);
+		
+		ListItem<Integer> eLst = new ListItem<>(9);
+		eLst.insert(4);
+		eLst.insert(5);
+		eLst.insert(3);
+		
+		Assert.assertTrue(eLst.equals(newLst));
 
 	}
 
 	@Test
 	public void insertSingleHead_Test_2() {
-
+		ListItem<Integer> lst= null;
+		
+		ListItem<Integer> newLst = a.insertSingleHead(lst,9);
+		
+		ListItem<Integer> eLst = new ListItem<>(9);
+		
+		Assert.assertTrue(eLst.equals(newLst));
 	}
 
 	@Test
 	public void insertSingleHead_Test_3() {
-
+		ListItem<Integer> lst= new ListItem<>(8);
+		
+		ListItem<Integer> newLst = a.insertSingleHead(lst,9);
+		
+		ListItem<Integer> eLst = new ListItem<>(8);
+		eLst.insert(9);
+		
+		Assert.assertTrue(eLst.equals(newLst));
 	}
 
 	/*****************************************************************
@@ -53,22 +110,47 @@ public class ATest {
 
 	@Test
 	public void removeElementsEqualX_Test_1() {
-
+		ListItem<Integer> lst= new ListItem<>(4);
+		lst.insert(5);
+		lst.insert(3);
+		lst.insert(2);
+		
+		ListItem<Integer> newLst = a.removeElementsEqualX(lst, 2, intValue());
+		
+		ListItem<Integer> eLst = new ListItem<>(4);
+		eLst.insert(5);
+		eLst.insert(3);
+		
+		Assert.assertTrue(eLst.equals(newLst));
 	}
-
+	
 	@Test
 	public void removeElementsEqualX_Test_2() {
-
+		ListItem<Integer> lst= new ListItem<>(4);
+		lst.insert(5);
+		lst.insert(3);
+		lst.insert(3);
+		lst.insert(2);
+		
+		ListItem<Integer> newLst = a.removeElementsEqualX(lst, 3, intValue());
+		
+		ListItem<Integer> eLst = new ListItem<>(4);
+		eLst.insert(5);
+		eLst.insert(2);
+		
+		Assert.assertTrue(eLst.equals(newLst));
 	}
 
-	@Test
+	@Test(expected = IllegalArgumentException.class)
 	public void removeElementsEqualX_Test_3() {
-
+		ListItem<Integer> lst= new ListItem<>(8);
+		a.removeElementsEqualX(lst, null, intValue());
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void removeElementsEqualX_Test_4() {
-
+		ListItem<Integer> lst= new ListItem<>(8);
+		a.removeElementsEqualX(lst, 5, null);
 	}
 
 	/*****************************************************************
@@ -77,17 +159,37 @@ public class ATest {
 
 	@Test
 	public void ringShiftLeft_Test_1() {
-
+		ListItem<Integer> lst= new ListItem<>(5);
+		lst.insert(4);
+		lst.insert(3);
+		lst.insert(2);
+		
+		ListItem<Integer> newLst = a.ringShiftLeft(lst);
+		ListItem<Integer> eLst = new ListItem<>(4);
+		eLst.insert(3);
+		eLst.insert(2);
+		eLst.insert(5);
+		
+		Assert.assertTrue(eLst.equals(newLst));
 	}
-
+	
 	@Test
 	public void ringShiftLeft_Test_2() {
-
+		ListItem<Integer> lst= null;
+		
+		Assert.assertNull(a.ringShiftLeft(lst));	
 	}
 
 	@Test
 	public void ringShiftLeft_Test_3() {
-
+		ListItem<Integer> lst= new ListItem<>(5);
+		lst.insert(4);
+		
+		ListItem<Integer> newLst = a.ringShiftLeft(lst);
+		ListItem<Integer> eLst = new ListItem<>(4);
+		eLst.insert(5);
+		
+		Assert.assertTrue(eLst.equals(newLst));
 	}
 
 	/*****************************************************************
@@ -96,22 +198,63 @@ public class ATest {
 
 	@Test
 	public void listsInList_Test_1() {
-
+		ListItem<ListItem<Integer>> lsts = new ListItem<ListItem<>>(95);
+		lst.insert(35);
+		lst.insert(11);
+		lst.insert(63);
+		
+		ListItem<Integer> newLst = a.listsInList(lsts);
+		
+		ListItem<Integer> eLst = new ListItem<>(95);
+		eLst.insert(35);
+		eLst.insert(11);
+		eLst.insert(63);
+		
+		assertTrue(eLst.equals(newLst));
+		
 	}
 
 	@Test
 	public void listsInList_Test_2() {
-
+		ListItem<ListItem<Integer>> lsts = new ListItem<ListItem<>>(9);
+		lst.insert(8);
+		lst.insert(7);
+		lst.insert(6);
+		lst.insert(5);
+		
+		ListItem<Integer> newLst = a.listsInList(lsts);
+		
+		ListItem<Integer> eLst = new ListItem<>(9);
+		eLst.insert(8);
+		eLst.insert(7);
+		eLst.insert(6);
+		eLst.insert(5);
+		
+		assertTrue(eLst.equals(newLst));
+		
 	}
 
 	@Test
 	public void listsInList_Test_3() {
-
+		ListItem<ListItem<Integer>> lsts = new ListItem<ListItem<>>(3);
+		lst.insert(2);
+		lst.insert(1);
+		
+		ListItem<Integer> newLst = a.listsInList(lsts);
+		
+		ListItem<Integer> eLst = new ListItem<>(3);
+		eLst.insert(2);
+		eLst.insert(1);
+		
+		assertTrue(eLst.equals(newLst));
+		
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void listsInList_Test_4() {
-
+		ListItem<ListItem<Integer>> lsts = null;
+		
+		a.listsInList(lsts);
 	}
 
 	/*****************************************************************
@@ -120,22 +263,34 @@ public class ATest {
 
 	@Test
 	public void arrayRunsToListOfLists_Test_1() {
-
+		Integer[] arr= {1,2,3};
+		
+		ListItem<Integer> newLst = a.arrayRunsToListOfLists(arr, intValue());
+		ListItem<ListItem<Integer>> elsts = new ListItem<ListItem<>>(1);
+		elsts.insert(2);
+		elsts.insert(3);
+		
+		assertTrue(eLsts.equals(newLst));
 	}
 
 	@Test
 	public void arrayRunsToListOfLists_Test_2() {
-
+		Integer[] arr= {};
+		
+		Assert.assertNull(a.arrayRunsToListOfLists(arr, intValue()));	
 	}
 
-	@Test
+	@Test(expected = IllegalArgumentException.class)
 	public void arrayRunsToListOfLists_Test_3() {
 
+		a.arrayRunsToListOfLists(null, intValue());
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void arrayRunsToListOfLists_Test_4() {
-
+		Integer[] arr= {1,2,3};
+		
+		a.arrayRunsToListOfLists(arr, null);
 	}
 
 	/*****************************************************************
@@ -144,16 +299,34 @@ public class ATest {
 
 	@Test
 	public void compare_Test_1() {
+		AngledGeometricElement s1 = new RectangleElement();
+		s1.changeColorCode(3);
+		AngledGeometricElement s2 = new RectangleElement();
+		Comparator s = new ComparatorColor();
+		 
+		Assert.assertTrue(s.compare(s1, s2) > 0);
 
 	}
 
 	@Test
 	public void compare_Test_2() {
+		AngledGeometricElement s1 = new RectangleElement();
+		AngledGeometricElement s2 = new RectangleElement();
+		Comparator s = new ComparatorColor();
+		
+		Assert.assertTrue(s.compare(s1, s2) == 0);
 
 	}
 
 	@Test
 	public void compare_Test_3() {
+		AngledGeometricElement s1 = new RectangleElement();
+		RoundGeometricElement c1 = new CircleElement();
+		c1.changeColorCode(6);
+		s1.changeColorCode(3);
+		Comparator s = new ComparatorColor();
+		  
+		Assert.assertTrue(s.compare(s1, c1) < 0);
 
 	}
 
@@ -163,16 +336,46 @@ public class ATest {
 
 	@Test
 	public void move_Test_1() {
+		Point[] points = new Point[]{new Point(0, 0), new Point(0, 1), new Point(1, 1), new Point(1, 0)};
+		AngledGeometricElement s1 = new SquareElement();
+		s1.setPoints(points);
+		s1.move(1, 1);
+		Point[] np = s1.getPoints();
+		  
+		Assert.assertTrue(np[0].getX() == 1 && np[0].getY() == 1);
+		Assert.assertTrue(np[1].getX() == 1 && np[1].getY() == 2);
+		Assert.assertTrue(np[2].getX() == 2 && np[2].getY() == 2);
+		Assert.assertTrue(np[3].getX() == 2 && np[3].getY() == 1);
 
 	}
 
 	@Test
 	public void move_Test_2() {
+		Point[] points = new Point[]{new Point(0, 0), new Point(0, 1), new Point(2, 1), new Point(2, 0)};
+		AngledGeometricElement s1 = new RectangleElement();
+		s1.setPoints(points);
+		s1.move(-1, 0);
+		Point[] np = s1.getPoints();
+		
+		Assert.assertTrue(np[0].getX() == -1 && np[0].getY() == 0);
+		Assert.assertTrue(np[1].getX() == -1 && np[1].getY() == 1);
+		Assert.assertTrue(np[2].getX() == 1 && np[2].getY() == 1);
+		Assert.assertTrue(np[3].getX() == 1 && np[3].getY() == 0);
 
 	}
 
 	@Test
 	public void move_Test_3() {
-
+		Point[] points = new Point[]{new Point(0, 0), new Point(0, 1), new Point(1, 1), new Point(1, 0)};
+		AngledGeometricElement s1 = new SquareElement();
+		s1.setPoints(points);
+		s1.move(0, 0);
+		Point[] np = s1.getPoints();
+		 
+		Assert.assertTrue(np[0].getX() == 0 && np[0].getY() == 0);
+		Assert.assertTrue(np[1].getX() == 0 && np[1].getY() == 1);
+		Assert.assertTrue(np[2].getX() == 1 && np[2].getY() == 1);
+		Assert.assertTrue(np[3].getX() == 1 && np[3].getY() == 0);
+		
 	}
 }
--- src/Aufgabe_2/DTest.java
+++ src/Aufgabe_2/DTest.java
@@ -1,31 +1,59 @@
 package Aufgabe_2;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.Assert;
 import org.junit.Test;
 
-public class DTest {
+import java.util.Comparator;
 
+import data.ListItem;
+import Aufgabe_1.D.D;
+import Aufgabe_1.D.ComparatorRadius;
+import Aufgabe_1.V.ComparatorInteger;
+import model.GeometricModelElement;
+import model.Point;
+import model.angled.AngledGeometricElement;
+import model.angled.SquareElement;
+import model.angled.RectangleElement;
+import model.round.CircleElement;
+
+public class DTest {
+ 
 	/*****************************************************************
 	 * switchElements
 	 *****************************************************************/
-
+	private D<Integer> d = new D<>();
+	private Integer[] arr1 = {54, 86, 234, 155};
+	private Integer[] arr2 = null;
+	
 	@Test
 	public void switchElements_Test_1() {
-
+		Integer[] sArr = d.switchElements(arr1, 0, 1);
+		Integer[] eArr = {86, 54, 234, 155};
+		
+		Assert.assertTrue(eArr.equals(sArr));
 	}
 
 	@Test
 	public void switchElements_Test_2() {
-
+		Integer[] sArr = d.switchElements(arr1, 2, 3);
+		Integer[] eArr = {54, 86, 155, 234};
+		
+		Assert.assertTrue(eArr.equals(sArr));
 	}
 
-	@Test
+	@Test(expected = IllegalArgumentException.class)
 	public void switchElements_Test_3() {
-
+		
+		d.switchElements(arr2, 0, 1);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void switchElements_Test_4() {
-
+		
+		d.switchElements(arr1, 7, 1);
 	}
 
 	/*****************************************************************
@@ -34,17 +62,49 @@ public class DTest {
 
 	@Test
 	public void insertLast_Test_1() {
+		ListItem<Integer> oneInt = new ListItem<>(3);
+		ListItem<Integer> twoInt = new ListItem<>(7);
+		ListItem<Integer> threeInt = new ListItem<>(7);
+		oneInt.next= twoInt;
+		twoInt.next= threeInt;
+		ListItem<Integer> fourInt = new ListItem<>(4);
+		ListItem<Integer> newlst = d.insertLast(oneInt, fourInt);
+		
+		ListItem<Integer> eLst = new ListItem<>(3);
+		eLst.insert(7);
+		eLst.insert(7);
+		eLst.insert(4);
 
-	}
+		Assert.assertTrue(eLst.equals(newlst));
+}
 
 	@Test
 	public void insertLast_Test_2() {
+		ListItem<Integer> oneInt = new ListItem<>(3);
+		ListItem<Integer> twoInt = new ListItem<>(7);
+		ListItem<Integer> threeInt = new ListItem<>(7);
+		oneInt.next= twoInt;
+		twoInt.next= threeInt;
+		
+		ListItem<Integer> fourInt = new ListItem<>(4);
+		ListItem<Integer> fiveInt = new ListItem<>(5);
+		fourInt.next=fiveInt;
+		ListItem<Integer> newlst = d.insertLast(oneInt, fourInt);
+		
+		ListItem<Integer> eLst = new ListItem<>(3);
+		eLst.insert(7);
+		eLst.insert(7);
+		eLst.insert(4);
+		eLst.insert(5);
 
+		Assert.assertTrue(eLst.equals(newlst));
 	}
 
 	@Test
 	public void insertLast_Test_3() {
-
+		ListItem<Integer> last = new ListItem<>(1);
+		ListItem<Integer> newlst = d.insertLast(null, last);
+		Assert.assertTrue(newlst.equals(new ListItem<>(1)));
 	}
 
 	/*****************************************************************
@@ -53,22 +113,50 @@ public class DTest {
 
 	@Test
 	public void removeSecMaxElement_Test_1() {
+		ListItem<Integer> lst = new ListItem<>(11);
+		lst.insert(22);
+		lst.insert(33);
+		lst.insert(44);
 
-	}
+		ListItem<Integer> newLst = d.removeSecMaxElement(lst,new ComparatorInteger());
+		
+		ListItem<Integer> eLst = new ListItem<>(11);
+		eLst.insert(22);
+		eLst.insert(44);
+		
+		Assert.assertTrue(eLst.equals(newLst));
+
+}
 
 	@Test
 	public void removeSecMaxElement_Test_2() {
+		ListItem<Integer> lst = new ListItem<>(11);
+		lst.insert(22);
+		lst.insert(22);
+		lst.insert(44);
 
+		ListItem<Integer> newLst = d.removeSecMaxElement(lst,new ComparatorInteger());
+		
+		ListItem<Integer> eLst = new ListItem<>(11);
+		eLst.insert(44);
+		
+		Assert.assertTrue(eLst.equals(newLst));
 	}
 
 	@Test
 	public void removeSecMaxElement_Test_3() {
-
+	
+	Assert.assertNull(d.removeSecMaxElement(null,new ComparatorInteger()));
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void removeSecMaxElement_Test_4() {
-
+		ListItem<Integer> lst = new ListItem<>(11);
+		lst.insert(22);
+		lst.insert(33);
+		lst.insert(44);
+		
+		d.removeSecMaxElement(lst,null);
 	}
 
 	/*****************************************************************
@@ -77,18 +165,44 @@ public class DTest {
 
 	@Test
 	public void invertTriples_Test_1() {
-
+		ListItem<Integer> lst = new ListItem<>(55);
+		oriLst.insert(44);
+		oriLst.insert(33);
+		oriLst.insert(22);
+		ListItem<Integer> newLst = d.invertTriples(lst);
+		
+		ListItem<Integer> eLst = new ListItem<>(33);
+		eLst.insert(44);
+		eLst.insert(55);
+		eLst.insert(22);
+		assertTrue(newLst.equals(eLst));
+	
 	}
 
 	@Test
 	public void invertTriples_Test_2() {
-
+		ListItem<Integer> lst = new ListItem<>(55);
+		oriLst.insert(44);
+		oriLst.insert(33);
+		oriLst.insert(22);
+		oriLst.insert(11);
+		oriLst.insert(66);
+		ListItem<Integer> newLst = d.invertTriples(lst);
+		
+		ListItem<Integer> eLst = new ListItem<>(33);
+		eLst.insert(44);
+		eLst.insert(55);
+		eLst.insert(66);
+		eLst.insert(11);
+		eLst.insert(22);
+		assertTrue(newLst.equals(eLst));
 	}
 
-	@Test
+	@Test(expected = IllegalArgumentException.class)
 	public void invertTriples_Test_3() {
-
-	}
+		
+		d.invertTriples(null);
+    }
 
 	/*****************************************************************
 	 * divideAlternatinglyIntoLists
@@ -96,16 +210,56 @@ public class DTest {
 
 	@Test
 	public void divideAlternatinglyIntoLists_Test_1() {
-
+		ListItem<Integer> lst = new ListItem<>(95);
+		lst.insert(5);
+		lst.insert(35);
+		lst.insert(2);
+		lst.insert(11);
+		lst.insert(24);
+		lst.insert(63);
+		lst.insert(77);
+		lst.insert(66);
+		
+		ListItem<ListItem<Integer>> newLst = d.divideAlternatinglyIntoLists(lst);
+		
+		ListItem<ListItem<Integer>> eLst = new ListItem<ListItem<>>(95);
+		eLst.insert(35);
+		eLst.insert(11);
+		eLst.insert(63);
+		eLst.insert(66);
+		eLst.insert(5);
+		eLst.insert(2);
+		eLst.insert(24);
+		eLst.insert(77);
+		
+		assertTrue(eLst.equals(newLst));
+		
 	}
 
 	@Test
 	public void divideAlternatinglyIntoLists_Test_2() {
-
+		ListItem<Integer> lst= new ListItem<>(1);
+		lst.insert(2);
+		lst.insert(3);
+		lst.insert(4);
+		
+		ListItem<ListItem<Integer>> newLst = d.divideAlternatinglyIntoLists(lst);
+		
+		ListItem<ListItem<Integer>> eLst = new ListItem<ListItem<>>(1);
+		eLst.insert(3);
+		eLst.insert(2);
+		eLst.insert(4);
+		
+		assertTrue(eLst.equals(newLst));	
 	}
 
 	@Test
 	public void divideAlternatinglyIntoLists_Test_3() {
+		ListItem<Integer> lst= new ListItem<>(5);
+	    ListItem<ListItem<Integer>> newLst = d.divideAlternatinglyIntoLists(lst);
+		ListItem<ListItem<Integer>> eLst = new ListItem<ListItem<>>(5);
+		
+		assertTrue(eLst.equals(newLst));	
 
 	}
 
@@ -115,22 +269,50 @@ public class DTest {
 
 	@Test
 	public void listIntoArray_Test_1() {
-
+		ListItem<Integer> lst= new ListItem<>(67);
+        Integer[] arr= d.listIntoArray(lst, intValue());
+		
+		Integer[] eArr= {67};
+		
+		for(int i=0;i<arr.length;i++) {
+			Assert.assertEquals(arr[i].intValue(),eArr[i].intValue());
+		}
 	}
 
 	@Test
 	public void listIntoArray_Test_2() {
-
-	}
-
-	@Test
-	public void listIntoArray_Test_3() {
-
+		ListItem<Integer> lst= new ListItem<>(5);
+		expectLst.insert(4);
+		expectLst.insert(3);
+		expectLst.insert(2);
+		expectLst.insert(1);
+		
+		Integer[] arr= d.listIntoArray(lst, intValue());
+		Integer[] eArr= {5,4,3,2,1};
+		
+		
+		for(int i=0;i<arr.length;i++) {
+			Assert.assertEquals(arr[i].intValue(),eArr[i].intValue());
+		}	
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void listIntoArray_Test_4() {
+	public void listIntoArray_Test_3() {
+		ListItem<Integer> lst = null;
+		
+		d.listIntoArray(lst, intValue());
+	}
 
+
+	@Test(expected = IllegalArgumentException.class)
+	public void listIntoArray_Test_4() {
+		ListItem<Integer> lst= new ListItem<>(5);
+		expectLst.insert(4);
+		expectLst.insert(3);
+		expectLst.insert(2);
+		expectLst.insert(1);
+		
+		d.listIntoArray(lst, null);
 	}
 
 	/*****************************************************************
@@ -139,16 +321,38 @@ public class DTest {
 
 	@Test
 	public void compare_Test_1() {
-
+		CircleElement c1 = new CircleElement();
+		c1.setHeight(2);
+		CircleElement c2 = new CircleElement();
+		c2.setHeight(2);
+		ComparatorRadius cmp = new ComparatorRadius();
+	
+		Assert.assertEquals(0, cmp.compare(c1, c2));
 	}
 
 	@Test
 	public void compare_Test_2() {
+		CircleElement c1 = new CircleElement();
+		c1.setStartPoint(new Point(1, 1));
+		c1.setHeight(2);
+		CircleElement c2 = new CircleElement();
+		c2.setHeight(12);
+		ComparatorRadius cmp = new ComparatorRadius();
+		
+		Assert.assertEquals(-1, cmp.compare(c1, c2));
 
 	}
 
 	@Test
 	public void compare_Test_3() {
+		CircleElement c1 = new CircleElement();
+		c1.setStartPoint(new Point(1, 1));
+		c1.setHeight(2);
+		CircleElement c2 = new CircleElement();
+		c2.setWidth(1);
+		ComparatorRadius cmp = new ComparatorRadius();
+		 
+		Assert.assertEquals(1, cmp.compare(c1, c2));
 
 	}
 
@@ -158,16 +362,40 @@ public class DTest {
 
 	@Test
 	public void rotate_Test_1() {
+	    AngledGeometricElement s1 = new SquareElement();
+		Point[] points = new Point[]{new Point(0, 0), new Point(0, 1), new Point(1, 1), new Point(1, 0)};
+		s1.setPoints(points);
+		s1.rotate(0);
+		Point[] newp = s1.getPoints();
+		  
+		Assert.assertTrue(newp[0].getX() == 0);
+		Assert.assertTrue(newp[2].getY() == 1);
 
 	}
 
 	@Test
 	public void rotate_Test_2() {
+		AngledGeometricElement s1 = new RectangleElement();
+		Point[] points = new Point[]{new Point(0, 0), new Point(0, 4), new Point(2, 4), new Point(2, 0)};
+		s1.setPoints(points);
+		s1.rotate(90);
+		Point[] newp = s1.getPoints();
+		  
+		Assert.assertTrue(newp[0].getX() == -1);
+		Assert.assertTrue(newp[2].getY() == 1);
 
 	}
 
 	@Test
 	public void rotate_Test_3() {
+		 AngledGeometricElement s1 = new SquareElement();
+		 Point[] points = new Point[]{new Point(0, 0), new Point(0, 1), new Point(1, 1), new Point(1, 0)};
+		 s1.setPoints(points);
+		 s1.rotate(3600);
+		 Point[] newp = s1.getPoints();
+		 
+		 Assert.assertTrue(newp[0].getX() == 0);
+		 Assert.assertTrue(newp[2].getY() == 1);
 
 	}
 }
